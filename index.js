b62d5391db5271a7026e587880780831d63fa07d
Volodymyr Kudriavtsev <vladimirkud@ukr.net>
2022-12-27 23:42:26 +0200

    done

---
+++ index.js
@@ -0,0 +1,29 @@
+const refs = {
+  themeToggle: document.querySelector(".theme-toggle"),
+  HTML: document.querySelector("html"),
+};
+
+const addDarkClassToHTML = () => {
+  try {
+    if (localStorage.getItem("theme") === "dark") {
+      refs.HTML.classList.add("dark");
+      refs.themeToggle.checked = true;
+    } else {
+      refs.HTML.classList.remove("dark");
+    }
+  } catch (err) {
+    return;
+  }
+};
+
+const onThemeToggleClick = (e) => {
+  localStorage.getItem("theme") === "dark"
+    ? localStorage.removeItem("theme")
+    : localStorage.setItem("theme", "dark");
+
+  addDarkClassToHTML();
+};
+
+refs.themeToggle.addEventListener("click", onThemeToggleClick);
+
+addDarkClassToHTML();
